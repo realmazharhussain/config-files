43f04fe13668ba9b32fbaed2c680ff9886ea56a8
Mazhar Hussain <mmazharhussainkgb1145@gmail.com>
2021-10-01 19:26:30 +0500

    major re-organization/re-factorization: part 2

--- misc/scripts/0-appimaged.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sudo ln -sfT /mnt/Data/Apps+/Linux/AppImages/installed /Applications
-sudo ln -sfT /mnt/Data/Apps+/Linux/AppImages/appimaged.AppImage /usr/local/bin/appimaged
-sudo ln -sft /etc/xdg/autostart/ /mnt/Data/Apps+/Linux/AppImages/autostart/appimagekit-appimaged.desktop
--- setup-all.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-./misc/setup.sh
-./install.sh

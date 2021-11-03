0c1b0e1f899793d325645f46f1984353d3989c0c
Mazhar Hussain <mmazharhussainkgb1145@gmail.com>
2021-11-03 11:12:49 +0500

    5-appimaged.sh: autostart -> systemd service

--- install-scripts/5-appimaged.sh
+++ install-scripts/5-appimaged.sh
@@ -2,4 +2,7 @@
 echo "Installing appimaged ..."
 sudo ln -sfT /mnt/Data/Apps+/Linux/AppImages/installed /Applications
 sudo ln -sfT /mnt/Data/Apps+/Linux/AppImages/appimaged.AppImage /usr/bin/appimaged
-sudo ln -sft /etc/xdg/autostart/ /mnt/Data/Apps+/Linux/AppImages/autostart/appimagekit-appimaged.desktop
+#sudo ln -sft /etc/xdg/autostart/ /mnt/Data/Apps+/Linux/AppImages/autostart/appimagekit-appimaged.desktop
+sudo ln -sft /etc/systemd/user/ /mnt/Data/Apps+/Linux/AppImages/systemd/user/appimaged.service
+echo "Enabling/Starting appimaged.service ..."
+systemctl --user enable --now appimaged.service

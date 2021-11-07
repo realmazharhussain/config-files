a86ecab42a4f80d2afa0ff046f1a6cfc3f0009cd
Mazhar Hussain <mmazharhussainkgb1145@gmail.com>
2021-11-07 11:20:38 +0500

    5-appimaged.sh: copy instead of symlinking

--- install-scripts/5-appimaged.sh
+++ install-scripts/5-appimaged.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "Installing appimaged ..."
 sudo ln -sfT /mnt/Data/Apps+/Linux/AppImages/installed /Applications
-sudo ln -sfT /mnt/Data/Apps+/Linux/AppImages/appimaged.AppImage /usr/bin/appimaged
-#sudo ln -sft /etc/xdg/autostart/ /mnt/Data/Apps+/Linux/AppImages/autostart/appimagekit-appimaged.desktop
-sudo ln -sft /etc/systemd/user/ /mnt/Data/Apps+/Linux/AppImages/systemd/user/appimaged.service
+sudo install -T /mnt/Data/Apps+/Linux/AppImages/appimaged.AppImage /usr/local/bin/appimaged
+#sudo ln -ft /etc/xdg/autostart/ /mnt/Data/Apps+/Linux/AppImages/autostart/appimagekit-appimaged.desktop
+sudo install -t /etc/systemd/user/ /mnt/Data/Apps+/Linux/AppImages/systemd/user/appimaged.service
 echo "Enabling/Starting appimaged.service ..."
 systemctl --user enable --now appimaged.service

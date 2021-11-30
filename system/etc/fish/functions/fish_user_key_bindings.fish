08f03f19937f61c581ee581e755f75adb73b8949
Mazhar Hussain <mmazharhussainkgb1145@gmail.com>
2021-11-30 15:48:17 +0500

    Fish: use vim keybindings

--- system/etc/fish/functions/fish_user_key_bindings.fish
+++ system/etc/fish/functions/fish_user_key_bindings.fish
@@ -1,15 +1,21 @@
 function fish_user_key_bindings
+  set -l mode insert
+
+  fish_default_key_bindings -M $mode
+
   # Control key bindings
-  bind \e\[1\;5D  backward-bigword         # Ctrl+⬅️
-  bind \e\[1\;5C  forward-bigword          # Ctrl+➡️
-  bind \b         backward-kill-bigword    # Ctrl+Backspace
-  bind \e\[3\;5~  kill-bigword             # Ctrl+Delete
+  bind -M $mode \e\[1\;5D  backward-bigword         # Ctrl+⬅️
+  bind -M $mode \e\[1\;5C  forward-bigword          # Ctrl+➡️
+  bind -M $mode \b         backward-kill-bigword    # Ctrl+Backspace
+  bind -M $mode \e\[3\;5~  kill-bigword             # Ctrl+Delete
   # Alternate key bindings
-  bind \e\[1\;3D  backward-word            # Alt+⬅️
-  bind \e\[1\;3C  forward-word             # Alt+➡️
-  bind \e\x7F     backward-kill-word       # Alt+Backspace
-  bind \e\[3\;3~  kill-word                # Alt+Delete
+  bind -M $mode \e\[1\;3D  backward-word            # Alt+⬅️
+  bind -M $mode \e\[1\;3C  forward-word             # Alt+➡️
+  bind -M $mode \e\x7F     backward-kill-word       # Alt+Backspace
+  bind -M $mode \e\[3\;3~  kill-word                # Alt+Delete
   # Disabled
-  #bind \cR        history-search-backward  # Ctrl+r
-  #bind \er        history-search-forward   # Alt+r
+  #bind -M $mode \cR        history-search-backward  # Ctrl+r
+  #bind -M $mode \er        history-search-forward   # Alt+r
+
+  fish_vi_key_bindings --no-erase $mode
 end
